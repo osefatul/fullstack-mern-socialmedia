5da2a61ab4f7d4caa2df032a13ffe05d434e1b77
osefatul <sefaullahomar2@gmail.com>
2021-11-05 01:47:01 -0700

    user login and registration are done

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -23,4 +23,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not found"); //if username is not valid
+
+    const valiPassword = await bcrypt.compare(req.body.password, user.password);
+    !valiPassword && res.status(404).json("Wrong password"); //if password is not valid
+
+    //if both are valid
+    res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 module.exports = router;
