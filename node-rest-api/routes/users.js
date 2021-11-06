650f355d46a40337372c99951c3f8e33244c9d51
osefatul <sefaullahomar2@gmail.com>
2021-11-05 23:43:48 -0700

    user get router is  done

--- node-rest-api/routes/users.js
+++ node-rest-api/routes/users.js
@@ -39,7 +39,18 @@ router.delete("/:id", async (req, res) => {
     res.status(403).json("You can only delete you Account!");
   }
 });
+
 //GET A USER
+router.get("/:id", async function (req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //FOLLOW A USER
 //UNFLOLLOW A USER
 
