28aa3aadf02d89b3704966fce103c4b7d2a6381a
osefatul <sefaullahomar2@gmail.com>
2021-11-14 03:30:46 -0800

    timeline all  commmands are being written

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -59,4 +59,17 @@ router.put("/:id/like", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Timeline post
+router.get("/timeline", async (req, res) => {});
 module.exports = router;
