df410ef6a16dc0c78800fedf6aeda0914b3c9762
osefatul <sefaullahomar2@gmail.com>
2021-11-17 03:04:23 -0800

    feed design is completed as of now

--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -19,22 +19,23 @@ function Share() {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia className="shareIcon" />
+              <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
             </div>
             <div className="shareOption">
-              <Label className="shareIcon" />
+              <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
             </div>
             <div className="shareOption">
-              <Room className="shareIcon" />
+              <Room htmlColor="green" className="shareIcon" />
               <span className="shareOptionText">Location</span>
             </div>
             <div className="shareOption">
-              <EmojiEmotions className="shareIcon" />
+              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
+          <button className="shareButton">Share</button>
         </div>
       </div>
     </div>
