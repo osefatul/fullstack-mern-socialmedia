31b56ebff19e08bd478f8816206f2f4abf8f5845
osefatul <sefaullahomar2@gmail.com>
2021-11-21 21:26:22 -0800

    feed design is completed as of now

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -8,7 +8,7 @@ function Post() {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src={blackWidow} alt="" />
+            <img src={blackWidow} alt="" className="postProfileImag" />
             <span className="postUsername">Black Widow</span>
             <span className="postDate">5 min ago</span>
           </div>
@@ -16,8 +16,24 @@ function Post() {
             <MoreVert />
           </div>
         </div>
-        <div className="postCenter"></div>
-        <div className="postBottom"></div>
+        <div className="postCenter">
+          <span className="postText">hey yo! look at this, who is here!</span>
+          <img
+            src="../../assets/post/wakandaWar.jpg"
+            alt=""
+            className="postImg"
+          />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="../../assets/like.png" alt="" className="likeIcon" />
+            <img src="../../assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">10 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
       </div>
     </div>
   );
