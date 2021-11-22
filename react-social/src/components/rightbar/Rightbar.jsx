7ee03bbe0b6533b182a96ea8885a411f7b4f5966
osefatul <sefaullahomar2@gmail.com>
2021-11-21 23:11:08 -0800

    rightbar page has been coded

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 import "./Rightbar.css";
 function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapp">
+        <div className="birthdayContainer">
+          <img
+            src="../../assets/birthdayGift.png"
+            alt=""
+            className="birthdayImg"
+          />
+          <span className="birthdayText">
+            <b>Thor</b> and <b>2 other friends</b> have a birthday.
+          </span>
+        </div>
+        <img src="../../assets/ad.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="../../assets/person/thor.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Thor</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="../../assets/person/captian.png"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Captian</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="../../assets/person/iron man.webp"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Iron Man</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
