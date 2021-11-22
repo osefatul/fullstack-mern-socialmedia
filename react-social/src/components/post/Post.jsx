9f9c09c2ad45ad5c21677b7b92446f6f2eab20c1
osefatul <sefaullahomar2@gmail.com>
2021-11-22 01:45:30 -0800

    feed has been updated

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.css";
 import blackWidow from "../../assets/person/blackWidow.jpg";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../data";
 function Post({ post }) {
-  console.log(post);
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
@@ -30,9 +36,19 @@ function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="../../assets/like.png" alt="" className="likeIcon" />
-            <img src="../../assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img
+              src="../../assets/like.png"
+              alt=""
+              className="likeIcon"
+              onClick={likeHandler}
+            />
+            <img
+              src="../../assets/heart.png"
+              alt=""
+              className="likeIcon"
+              onClick={likeHandler}
+            />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
