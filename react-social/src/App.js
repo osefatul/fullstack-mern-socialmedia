c43d4530c04d0e225ac68199137481fd6f7b6438
osefatul <sefaullahomar2@gmail.com>
2021-11-22 05:24:13 -0800

    Login page

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./pages/home/Home";
-import Profile from "./pages/Profile/Profile";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Login />
     </div>
   );
 }
