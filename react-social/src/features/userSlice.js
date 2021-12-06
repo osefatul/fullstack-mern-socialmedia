758bc281a33441dc2299bf898902692ecdfb4a32
osefatul <sefaullahomar2@gmail.com>
2021-12-06 05:39:18 -0800

    working on manaement tool such as redux

---
+++ react-social/src/features/userSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  // 1/2 EXTREMELLY IMPORTANT FOR LOGIN PERSISTATION
+  user: JSON.parse(localStorage.getItem("userInfo")) || null,
+  isFetching: false,
+  error: false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  //So what are the actions for redux we want to deploy.
+  reducers: {
+    //This is how we can dispatch loginFailure in the login page: ------> dispatch(login(data))
+    //then the data is assigned to the "user" in the state.
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state, action) => {
+      state.user = null; // means there should be no data of any user
+    },
+    credentialsFetched: (state, action) => {
+      state.isFetching = true;
+    },
+    loginError: (state, action) => {
+      //This is how we can dispatch loginFailure in the login page: ------>    dispatch(loginFailure())
+      state.error = true;
+    },
+  },
+});
+
+export const { login, logout, credentialsFetched, loginError } =
+  userSlice.actions;
+
+// The function below is called a selector and allows us to select a value from the state. or send export the state value
+export const selectUser = (state) => state.user.user;
+export const selectError = (state) => state.user.error;
+export const selectIsFetching = (state) => state.user.isFetching;
+
+export default userSlice.reducer;
--- react-social/src/pages/login/Login.jsx
+++ react-social/src/pages/login/Login.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./login.css";
 
 function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -12,13 +18,25 @@ function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleSubmit}>
+            <input
+              placeholder="Email"
+              className="loginInput"
+              ref={emailRef}
+              required
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+              ref={passwordRef}
+              required
+              minLength="6"
+            />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">Creat a new account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
