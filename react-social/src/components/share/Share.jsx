96898e96a0650d601448d56db4a19fb72d46244f
osefatul <sefaullahomar2@gmail.com>
2021-12-12 17:16:05 -0800

    share post is updated

--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Share.css";
 
 import { EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons";
@@ -9,6 +9,25 @@ import { Link } from "react-router-dom";
 function Share() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const user = useSelector(selectUser);
+
+  //for post input description and file
+  const desc = useRef();
+  const [file, setFile] = useState(null);
+
+  //submitform
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    try {
+      await axios.post("/posts", newPost);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="share">
@@ -29,15 +48,25 @@ function Share() {
             className="shareInput"
             type="text"
             placeholder={`What is in your mind ${user.username}?`}
+            ref={desc}
           />
         </div>
         <hr className="shareHr" />
-        <div className="shareBottom">
+        <form className="shareBottom" onSubmit={handleSubmit}>
           <div className="shareOptions">
-            <div className="shareOption">
+            <label htmlFor="file" className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-            </div>
+              <input
+                type="file"
+                id="file"
+                // accept only files with below extensions.
+                accept=".png, .jpg, .jpeg"
+                // we dont want choos all filess. just choose the first file.
+                onChange={(e) => setFile(e.target.files[0])}
+                style={{ display: "none" }}
+              />
+            </label>
             <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -51,8 +80,10 @@ function Share() {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
-          <button className="shareButton">Share</button>
-        </div>
+          <button className="shareButton" type="submit">
+            Share
+          </button>
+        </form>
       </div>
     </div>
   );
