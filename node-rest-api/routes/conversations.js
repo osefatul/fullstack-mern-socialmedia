5e1d4bbcea422ff795a1d8c3e9ae19492c1dfe83
osefatul <sefaullahomar2@gmail.com>
2021-12-30 23:14:42 -0800

    it is done

--- node-rest-api/routes/conversations.js
+++ node-rest-api/routes/conversations.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Conversation = require("../models/Conversation");
 
 //New Conversation
-
 router.post("/", async (req, res) => {
   const newConversation = new Conversation({
     members: [req.body.senderId, req.body.receiverId],
@@ -11,6 +10,19 @@ router.post("/", async (req, res) => {
   try {
     const savedConversation = await newConversation.save();
     res.status(200).json(savedConversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GEt Conversationd of the a user
+router.get("/:userId", async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] }, //show us all conversation that the userId in included in.
+    });
+
+    res.status(200).json(conversation);
   } catch (err) {
     res.status(500).json(err);
   }
--- node-rest-api/routes/messages.js
+++ node-rest-api/routes/messages.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
-const Message = require("../models/Conversation");
+const Message = require("../models/Message");
+
+//Create a new Message
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get all messages that belong & happened between two people using conversation id
+router.get("/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
